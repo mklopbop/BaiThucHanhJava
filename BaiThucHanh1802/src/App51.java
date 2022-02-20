05e8ce6fe6dd23b0e41f1e226f20a055575d98aa
mklopbop <sboyy02@gmail.com>
2022-02-20 12:20:15 +0700

    first commit

---
+++ BaiThucHanh1802/src/App51.java
@@ -0,0 +1,13 @@
+package baitapjava.src;
+
+public class App51 {
+    public static void main(String[] args){
+        System.out.println("Cac so nho hon 5 la:");
+            int number = 0 ;
+            for (;number < 5;){
+                System.out.println(number );
+                number++;
+            }
+    }
+    
+}
